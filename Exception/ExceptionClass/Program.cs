442adf31b4428a9763826e3a46bdaee24b7afb90
Chhiring Lama <lamachhiring103@gmail.com>
2024-02-02 00:34:01 +0545

    Exception Update

---
+++ Exception/ExceptionClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Mynamespace {
+    class Program {
+        static void Main(string[] args) {
+
+            int x=10;
+            try{
+                if(x==10){
+                    throw new ArgumentOutOfRangeException();
+            }
+            }
+            catch{
+                Console.WriteLine("Error generated!! Age is set="+x);
+            }
+        }
+    }
+}
